39416a714f99467499f37850bb6447231441f84d
Dungdd <daoducdung@ginno.com>
2022-10-26 16:39:34 +0700

    Xây dựng Ui phần Header #2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://i.insider.com/62a100ec2253ea001951c9cb?width=1104&format=jpeg"
+                alt="Hoa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
